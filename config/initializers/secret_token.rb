4c5d0eddb7bf67fe33fe50a6447df2aad02411bc
Lucas Caton <lucascaton@gmail.com>
2011-09-11 14:13:00 -0300

    Initial Rails 3.1 structure directory

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Selfsp::Application.config.secret_token = '51c90375c110aae4de9c43b4ab7f04b1dd93122937cb99ae9bdae374c556f83894f87888b0e823e8261a2a65fe29a22dc657bb7aac4e17502da69b06992cb9b2'
